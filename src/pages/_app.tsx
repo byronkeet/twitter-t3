51e247c9edeb7ab9304e9022e80a0506b69dae39
Byron Keet <keetbis@gmail.com>
2023-07-11 08:23:33 +0200

    Add profile route, update index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,29 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import Head from "next/head";
+
+import SideNav from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+	return (
+	<SessionProvider session={session}>
+		<Head>
+			<title>Twitter T3</title>
+			<meta name="description" content="Twitter clone using T3 stack" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<div className="container mx-auto flex items-start sm:pr-4">
+			<SideNav />
+			<div className="min-h-screen flex-grow border-x">
+				<Component {...pageProps} />
+			</div>
+		</div>
+	</SessionProvider>
+	);
 };
 
 export default api.withTRPC(MyApp);
